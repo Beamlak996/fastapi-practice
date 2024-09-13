567994bde35463de16d18df052b8776c69240d5e
Beamlak996 <beamlaktsegaye996@gmail.com>
2024-09-13 08:20:43 -0900

    update and delete done

--- router/user.py
+++ router/user.py
@@ -24,3 +24,13 @@ def get_all_users(db: Session = Depends(get_db)):
 @router.get("/{id}", response_model=UserDisplay)
 def get_user(id:int, db: Session = Depends(get_db)):
     return db_user.get_user(db, id)
+
+# Update a user
+@router.post("/{id}/update")
+def update_user(id: int, request: UserBase, db: Session = Depends(get_db)):
+    return db_user.update_user(db, id, request)
+
+# Delete a user
+@router.get('/delete/{id}')
+def delete_user(id: int, db: Session = Depends(get_all_users)):
+    return db_user.delete_user(db, id)
