047c39886bfa7b5475f5d7651ebe68d11c945dad
Beamlak996 <beamlaktsegaye996@gmail.com>
2024-09-13 07:48:34 -0900

    create user on db done

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from router import blog_get
 from router import blog_post
+from router import user
 from db import models
 from db.database import engine
 
@@ -8,6 +9,7 @@ app = FastAPI()
 
 app.include_router(blog_get.router)
 app.include_router(blog_post.router)
+app.include_router(user.router)
 
 @app.get("/hell0")
 def index():
