3c3ef9974a60be28ef8c2d27cf2583103cd724b3
Beamlak996 <beamlaktsegaye996@gmail.com>
2024-09-13 08:03:42 -0900

    read user done

--- router/user.py
+++ router/user.py
@@ -3,6 +3,7 @@ from schemas import UserBase, UserDisplay
 from sqlalchemy.orm import Session
 from db.database import get_db
 from db import db_user
+from typing import List
 
 router = APIRouter(
     prefix='/user',
@@ -13,3 +14,13 @@ router = APIRouter(
 @router.post("/", response_model=UserDisplay)
 def create_user(request: UserBase, db: Session = Depends(get_db)):
     return db_user.create_user(db, request)
+
+# Get all users
+@router.get("/", response_model=List[UserDisplay])
+def get_all_users(db: Session = Depends(get_db)):
+    return db_user.get_all_users(db)
+
+# Get a user
+@router.get("/{id}", response_model=UserDisplay)
+def get_user(id:int, db: Session = Depends(get_db)):
+    return db_user.get_user(db, id)
